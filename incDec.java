8230ee37599c47a2ea7391648c2fd004370ce739
mahammedaadil patel <adilchoice30@gmail.com>
2025-11-25 17:42:15 +0530

    increament decreament

---
+++ incDec.java
@@ -0,0 +1,15 @@
+public class incDec {
+
+    public static void main(String[] args) {
+        //increament and decreament
+
+        int a=10;
+        int b= a++;
+        int c=a;
+        System.out.println(a+" "+c);
+        int d=--a;
+        int e=c--;
+
+        System.out.println(d+" "+e);
+    }
+}
